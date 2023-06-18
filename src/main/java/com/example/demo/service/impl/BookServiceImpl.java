95ff170560a520959edc6c94276f72e24e84fc70
wimlim <wimlim33@gmail.com>
2023-06-19 01:06:54 +0800

    change sql

--- src/main/java/com/example/demo/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BookServiceImpl.java
@@ -29,7 +29,12 @@ public class BookServiceImpl implements BookService {
             JSONObject bookJson = new JSONObject();
             bookJson.put("id", book.getId());
             bookJson.put("title", book.getTitle());
+            bookJson.put("author", book.getAuthor());
+            bookJson.put("language", book.getLanguage());
+            bookJson.put("isbn", book.getIsbn());
             bookJson.put("price", book.getPrice());
+            bookJson.put("stock", book.getStock());
+            bookJson.put("description", book.getDescription());
             booksJson.add(bookJson);
         }
         return booksJson.toJSONString();
